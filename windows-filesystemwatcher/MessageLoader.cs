2aa065f2cb45b7ed20fe7abf048aa267f7af175d
Javier Sanz Sánchez <javiersanzsanchez@gmail.com>
2022-09-08 17:06:39 +0200

    store queue message

---
+++ windows-filesystemwatcher/MessageLoader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Messaging;
+using System.IO;
+
+namespace windows_filesystemwatcher
+{
+    public class MessageLoader
+    {
+
+        private const String privateQueuePath = ".\\Private$\\windows-filesystemwatcher-queue";
+
+        private MessageQueue myQueue = null;
+
+        public MessageLoader()
+        {
+            myQueue = new MessageQueue(privateQueuePath);
+        }
+
+        // References private queues.
+        public void SendPrivate()
+        {
+            myQueue.Send("Private queue by path name.");
+        }
+
+        // References private queues.
+        public void SendPrivate(FileSystemEventArgs e)
+        {
+            myQueue.Send(Newtonsoft.Json.JsonConvert.SerializeObject(e));
+        }
+
+
+    }
+}
---
+++ windows-filesystemwatcher/WatcherQueueMessage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace windows_filesystemwatcher
+{
+    public class WatcherQueueMessage
+    {
+
+        public String Guid = "";
+        public String currentName = "";
+        public String previousName = "";
+        public String currentParentPath = "";
+        public String previousParentPath = "";
+        public String fullPath = "";
+        public String previousPath = "";
+        public String watcherEvent = "";
+        private DateTime watcherEventTime;
+
+        public WatcherQueueMessage()
+        {
+            watcherEventTime = DateTime.Now;
+        }
+
+        public String Serialized()
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(this);
+        }
+
+    }
+}
